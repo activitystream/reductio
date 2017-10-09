1eec600af3e5a1792e127e65e2eb33fbd2e74410
Einar Norðfjörð <einar@vortex.is>
2017-10-09 15:43:20 +0000

    any reducer

---
+++ src/any.js
@@ -0,0 +1,24 @@
+var reductio_any = {
+	add: function(a, prior, path) {
+		return function(p, v, nf) {
+			if (prior) prior(p, v, nf);
+			if (!path(p).any) path(p).any = a(v);
+			return p;
+		};
+	},
+	remove: function(a, prior, path) {
+		return function(p, v, nf) {
+			if (prior) prior(p, v, nf);
+			return p;
+		};
+	},
+	initial: function(prior, path) {
+		return function(p) {
+			p = prior(p);
+			p.any = '';
+			return p;
+		};
+	},
+};
+
+module.exports = reductio_any;
